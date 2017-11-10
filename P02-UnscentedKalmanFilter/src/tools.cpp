840e29e6a8d7cc2b34e39c82b926899f9aac866b
Jack Olivieri <boldorider4@gmail.com>
2017-11-11 00:53:57 +0100

    [P02] feat: Implement RMSE function

--- P02-UnscentedKalmanFilter/src/tools.cpp
+++ P02-UnscentedKalmanFilter/src/tools.cpp
@@ -15,4 +15,29 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  if (!estimations.size() || estimations.size() != ground_truth.size()) {
+    cout << "estimation size is zero or different than ground truth" << endl;
+  }
+
+  //accumulate squared residuals
+  for(size_t idx = 0; idx < estimations.size(); idx++) {
+    VectorXd residual = estimations[idx]-ground_truth[idx];
+    residual = residual.array() * residual.array();
+    rmse += residual;
+  }
+
+  //calculate the mean
+  rmse = rmse/estimations.size();
+
+  //calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  //return the result
+  return rmse;
 }
