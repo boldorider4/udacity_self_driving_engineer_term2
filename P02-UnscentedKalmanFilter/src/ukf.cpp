f0b434261474d9beed49e8207c5af4601a2971ab
Jack Olivieri <boldorider4@gmail.com>
2017-11-12 15:09:19 +0100

    [P02] feat: Tune expcted deviations of a and yadd

--- P02-UnscentedKalmanFilter/src/ukf.cpp
+++ P02-UnscentedKalmanFilter/src/ukf.cpp
@@ -61,10 +61,10 @@ UKF::UKF() {
   }
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 6;
+  std_a_ = 3;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 10;
+  std_yawdd_ = 1.2;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
